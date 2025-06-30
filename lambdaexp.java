7aec93c2358b9f2782315b69e67f2063a756a49d
harshittripathi <harshittripathi.dev@gmail.com>
2025-06-30 21:07:30 +0530

    day2 lambda exp

--- lambdaexp.java
+++ lambdaexp.java
@@ -1,5 +1,14 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.BiPredicate;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class lambdaexp {
 
@@ -50,6 +59,76 @@ public class lambdaexp {
     Function<Integer, Integer> quad = a -> a * a * a * a;
     System.out.println("quad:" + quad.apply(5));
 
+    // day 2
+
+    Function<Integer, Integer> doubleIt = x -> x * 2;
+    Function<Integer, Integer> tripleIt = x -> x * 3;
+
+    System.out.println("Double then Triple:" + doubleIt.andThen(tripleIt).apply(5));
+    System.out.println("Triple and then Double:" + tripleIt.andThen(doubleIt).apply(10));// same
+    System.out.println("Triple it and then double it:" + doubleIt.compose(tripleIt).apply(10));// same
+
+    // Consumer - accepts parameter but no return type
+    Consumer<Integer> printConsumer = x -> System.out.println(x);
+    printConsumer.accept(50);
+
+    List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
+    Consumer<List<Integer>> printList = x -> {
+      for (Integer integer : x) {
+        System.out.println(integer);
+      }
+    };
+    printList.accept(list);
+
+    // Supplier
+
+    Supplier<String> supplier = () -> "HelloWorld";
+    System.out.println(supplier.get());
+
+    // combined example
+
+    Predicate<Integer> isEven = x -> x % 2 == 0;
+    Function<Integer, Integer> square = x -> x * x;
+    Consumer<Integer> consumer = x -> System.out.println(x);
+    Supplier<Integer> supplier2 = () -> 100;
+
+    if (isEven.test(supplier2.get())) {
+      consumer.accept(square.apply(supplier2.get()));
+    }
+
+    // BiPredicate , BiConsumer, BiFunction
+
+    BiPredicate<Integer, Integer> isSumEven = (x, y) -> (x + y) % 2 == 0;
+    BiConsumer<Integer, Integer> consumer2 = (x, y) -> {
+      System.out.println(x);
+      System.out.println(y);
+    };
+    BiFunction<String, String, Integer> combinedLength = (x, y) -> (x + y).length();
+
+    System.out.println(isSumEven.test(5, 5));
+    consumer2.accept(45, 24);
+    System.out.println(combinedLength.apply("hat", "hack"));
+
+    // Method Reference -- use method without invoking in place of lambda
+
+    List<String> listOfNames = Arrays.asList("Ram", "Shyam", "Ghanshyam");
+    listOfNames.forEach(x -> System.out.println(x));
+    listOfNames.forEach(System.out::println); // method reference
+
+    // constructor reference
+
+    List<String> mobilePhoneStrings = Arrays.asList("Lava", "OnePlus", "Vivo");
+    List<MobilePhones> lMobilePhones = mobilePhoneStrings.stream().map(MobilePhones::new)
+        .collect(Collectors.toList());
+
+  }
+}
+
+class MobilePhones {
+  String names;
+
+  public MobilePhones(String names) {
+    this.names = names;
   }
 
 }
