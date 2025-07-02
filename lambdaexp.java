dd90dafe72df31d1157bba0798805cee63be5d6a
harshittripathi <harshittripathi.dev@gmail.com>
2025-07-02 21:45:48 +0530

    .

--- lambdaexp.java
+++ lambdaexp.java
@@ -7,7 +7,6 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class lambdaexp {
